e41e078441869fbdc79783c6a29f487e6dcf4338
clackypaas <clackypaas>
2024-08-16 19:11:41 +0800

    chore: update configuration files and modify EmailSection component

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,7 +56,7 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="gitlab.com">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
           <Link href="linkedin.com">
